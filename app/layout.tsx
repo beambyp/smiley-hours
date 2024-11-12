f30cbbfc4dc10b2ba55053f8588cd5269077a86d
mickymoucs <mkycskwcst@gmail.com>
2024-11-12 22:59:41 +0700

    Fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import { Anuphan, Montserrat, Akshar } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const montserrat = Montserrat({
   weight: ['500', '700'],
@@ -45,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} ${akshar.variable} ${anuphan.variable} antialiased`}
+        className={`${montserrat.variable} ${akshar.variable} ${anuphan.variable} antialiased`}
       >
         {children}
       </body>
