252e2b703ad123f18f73f862972b62a68313138e
mickymoucs <mkycskwcst@gmail.com>
2025-01-29 15:19:56 +0700

    fix the time deployment

--- app/api/medicalrecord/user/route.ts
+++ app/api/medicalrecord/user/route.ts
@@ -36,14 +36,16 @@ export async function GET(request: Request) {
                     month: "long",
                     year: "numeric",
                 }), 
-                treatmentStartTime: availableDateStart.toLocaleTimeString("th-TH", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                }),
-                treatmentEndTime: availableDateEnd.toLocaleTimeString("th-TH", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                }),
+                // treatmentStartTime: availableDateStart.toLocaleTimeString("th-TH", {
+                //     hour: "2-digit",
+                //     minute: "2-digit",
+                // }),
+                // treatmentEndTime: availableDateEnd.toLocaleTimeString("th-TH", {
+                //     hour: "2-digit",
+                //     minute: "2-digit",
+                // }),
+                treatmentStartTime: `${availableDateStart.getHours().toString().padStart(2, '0')}:${availableDateStart.getMinutes().toString().padStart(2, '0')}`,  
+                treatmentEndTime: `${availableDateEnd.getHours().toString().padStart(2, '0')}:${availableDateEnd.getMinutes().toString().padStart(2, '0')}`,      
                 };
         });
         return new Response(JSON.stringify(res), { status: 200 });
